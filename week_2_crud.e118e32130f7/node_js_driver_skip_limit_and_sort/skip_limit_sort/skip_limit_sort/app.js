a7fc599b238f97af320e6fd708e88933c94f1e56
Haibin Wang <hbwang@Haibin-Wang.local>
2015-08-24 17:03:59 +0800

    first notes

---
+++ week_2_crud.e118e32130f7/node_js_driver_skip_limit_and_sort/skip_limit_sort/skip_limit_sort/app.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+    if(err) throw err;
+
+    var grades = db.collection('grades');
+
+//无论skip， limit， sort位置，执行顺序为sort-->skip-->limit
+    var cursor = grades.find({});
+    cursor.skip(1);
+    cursor.limit(4);
+    cursor.sort('grade', 1);
+    //cursor.sort([['grade', 1], ['student', -1]]);
+
+    //var options = { 'skip' : 1,
+    //                'limit' : 4,
+    //                'sort' : [['grade', 1], ['student', -1]] };
+    //var cursor = grades.find({}, {}, options);
+
+    cursor.each(function(err, doc) {
+        if(err) throw err;
+        if(doc == null) {
+            return db.close();
+        }
+        console.dir(doc);
+    });
+});
